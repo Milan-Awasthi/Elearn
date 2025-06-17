e513d97931074a906860b05bc6c2e474e9cf40ce
Milan-Awasthi <milanawasthi1990@gmail.com>
2025-06-17 17:59:09 +0530

    Update Elearn.tf

--- Elearn.tf
+++ Elearn.tf
@@ -11,8 +11,8 @@ provider "azurerm" {
   subscription_id = "799b8ac2-95c2-49ae-bf18-9a3bf3936b19"
 
 }
-resource "azurerm_resource_group" "Milan_rg1" {
-  name     = "Milan_rg1"
+resource "azurerm_resource_group" "akhi_rg1" {
+  name     = "akhi_rg1"
   location = "Central India"
 
 }
