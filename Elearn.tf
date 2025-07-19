8c4f4ffbdf8f86958679468b805fb66cfa3d3751
Milan-Awasthi <milanawasthi1990@gmail.com>
2025-07-19 14:56:32 +0530

    Update Elearn.tf

--- Elearn.tf
+++ Elearn.tf
@@ -24,20 +24,20 @@ resource "azurerm_resource_group" "milan_rg1" {
 resource "azurerm_virtual_network" "vnet_Milan" {
   name                = "MilanVnet"
   address_space       = ["10.16.0.0/27"]
-  location            = azurerm_resource_group.Milan_rg1.location
-  resource_group_name = azurerm_resource_group.Milan_rg1.name
+  location            = azurerm_resource_group.milan_rg1.location
+  resource_group_name = azurerm_resource_group.milan_rg1.name
 }
 
 resource "azurerm_subnet" "subnet_frontend" {
   name                 = "frontend_Milan"
-  resource_group_name  = azurerm_resource_group.Milan_rg1.name
+  resource_group_name  = azurerm_resource_group.milan_rg1.name
   virtual_network_name = azurerm_virtual_network.vnet_Milan.name
   address_prefixes     = ["10.16.0.0/28"]
   depends_on           = [azurerm_virtual_network.vnet_Milan]
 }
 resource "azurerm_subnet" "subnet_backend" {
   name                 = "backend_Milan"
-  resource_group_name  = azurerm_resource_group.Milan_rg1.name
+  resource_group_name  = azurerm_resource_group.milan_rg1.name
   virtual_network_name = azurerm_virtual_network.vnet_Milan.name
   address_prefixes     = ["10.16.0.16/28"]
   depends_on           = [azurerm_virtual_network.vnet_Milan]
@@ -50,16 +50,16 @@ resource "azurerm_subnet" "subnet_backend" {
 #########################
 resource "azurerm_public_ip" "frontend_ip" {
   name                = "frontend-public-ip"
-  location            = azurerm_resource_group.Milan_rg1.location
-  resource_group_name = azurerm_resource_group.Milan_rg1.name
+  location            = azurerm_resource_group.milan_rg1.location
+  resource_group_name = azurerm_resource_group.milan_rg1.name
   allocation_method   = "Static"
   sku                 = "Basic"
 }
 
 resource "azurerm_public_ip" "backend_ip" {
   name                = "backend-public-ip"
-  location            = azurerm_resource_group.Milan_rg1.location
-  resource_group_name = azurerm_resource_group.Milan_rg1.name
+  location            = azurerm_resource_group.milan_rg1.location
+  resource_group_name = azurerm_resource_group.milan_rg1.name
   allocation_method   = "Static"
   sku                 = "Basic"
 }
@@ -69,8 +69,8 @@ resource "azurerm_public_ip" "backend_ip" {
 #########################
 resource "azurerm_network_interface" "nic_frontend" {
   name                = "nic-frontend-Milan"
-  location            = azurerm_resource_group.Milan_rg1.location
-  resource_group_name = azurerm_resource_group.Milan_rg1.name
+  location            = azurerm_resource_group.milan_rg1.location
+  resource_group_name = azurerm_resource_group.milan_rg1.name
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -82,8 +82,8 @@ resource "azurerm_network_interface" "nic_frontend" {
 
 resource "azurerm_network_interface" "nic_backend" {
   name                = "nic-backend-Milan"
-  location            = azurerm_resource_group.Milan_rg1.location
-  resource_group_name = azurerm_resource_group.Milan_rg1.name
+  location            = azurerm_resource_group.milan_rg1.location
+  resource_group_name = azurerm_resource_group.milan_rg1.name
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -106,8 +106,8 @@ variable "admin_password" {
 
 resource "azurerm_linux_virtual_machine" "frontend_vm" {
   name                            = "frontend-vm-Milan"
-  location                        = azurerm_resource_group.Milan_rg1.location
-  resource_group_name             = azurerm_resource_group.Milan_rg1.name
+  location                        = azurerm_resource_group.milan_rg1.location
+  resource_group_name             = azurerm_resource_group.milan_rg1.name
   size                            = "Standard_B1s"
   admin_username                  = var.admin_username
   admin_password                  = var.admin_password
@@ -138,8 +138,8 @@ resource "azurerm_linux_virtual_machine" "frontend_vm" {
 
 resource "azurerm_linux_virtual_machine" "backend_vm" {
   name                            = "backend-vm-Milan"
-  location                        = azurerm_resource_group.Milan_rg1.location
-  resource_group_name             = azurerm_resource_group.Milan_rg1.name
+  location                        = azurerm_resource_group.milan_rg1.location
+  resource_group_name             = azurerm_resource_group.milan_rg1.name
   size                            = "Standard_B1s"
   admin_username                  = var.admin_username
   admin_password                  = var.admin_password
